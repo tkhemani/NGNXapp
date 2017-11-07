7f23db8854ccad367ddbbb15625d35a1e26ee8ad
Tarun Khemani <tkhemani@gmail.com>
2017-11-07 05:39:50 +0530

    myapp scaffold

---
+++ apps/myapp/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
